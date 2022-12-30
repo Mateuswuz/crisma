d39f7a91666b94ae1845483c751c8d520e335106
Mateuzinholol <116360372+Mateuswuz@users.noreply.github.com>
2022-12-30 17:21:47 -0300

    Add files via upload

---
+++ assets/preload/stages/Krima.lua
@@ -0,0 +1,26 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('Wreath', 'XMAS/Court_Wreath', -1300, -0);
+	setScrollFactor('Wreath', 1.0, 1.0);
+
+	makeLuaSprite('FoodCourt', 'XMAS/Court_Foreground', -1300, -0);
+	setScrollFactor('FoodCourt', 1.0, 1.0);
+
+	makeLuaSprite('BarstoolFellas', 'XMAS/Xmas_Characters', -1300, -0);
+	setScrollFactor('BarstoolFellas', 1.0, 1.0);
+
+	makeLuaSprite('Lights', 'XMAS/Xmas_Frontlights', -1300, -0);
+	setScrollFactor('Lights', 1.0, 1.0);
+
+	makeLuaSprite('UpfrontFellas', 'XMAS/Xmas_FG_Foreground', -1300, -0);
+	setScrollFactor('UpfrontFellas', 0.9, 0.9);
+
+
+	addLuaSprite('FoodCourt', false);
+	addLuaSprite('BarstoolFellas', false);
+	addLuaSprite('Wreath', false);
+	addLuaSprite('Lights', true);
+	addLuaSprite('UpfrontFellas', true);
+	
+	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end
